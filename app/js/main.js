86c7f256adc3a2e0a708e84cf164e849d3adf5e0
Ihor Khrystiuk <inanutshell86@gmail.com>
2017-03-10 20:36:14 +0200

    add sprite with icon images

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,12 @@
 var btnBurger = document.querySelector('.main-nav__toggle');
 var menu = document.querySelector('.main-nav__wrapper');
+var btnUser = document.querySelector('.main-nav__user-login');
+var mfPersonal = document.querySelector('.modal-personal');
+var btnClose = document.querySelector('.btn--close');
 
 if (btnBurger) btnBurger.addEventListener('click', toggleMenu);
+if (btnUser) btnUser.addEventListener('click', showPersonal);
+if (btnClose) btnClose.addEventListener('click', closeModalForm);
 
 function toggleMenu() {
 
@@ -17,5 +22,23 @@ function toggleMenu() {
     menu.classList.remove('main-nav__wrapper--opened');
     menu.classList.add('main-nav__wrapper--closed');
   }
+
+}
+
+function showPersonal(event) {
+  event.preventDefault();
+
+  btnBurger.classList.remove('main-nav__toggle--opened');
+  btnBurger.classList.add('main-nav__toggle--closed');
+  menu.classList.remove('main-nav__wrapper--opened');
+  menu.classList.add('main-nav__wrapper--closed');
+
+  mfPersonal.classList.add('modal-personal--show');
+}
+
+function closeModalForm(event) {
+  event.preventDefault();
+
+  mfPersonal.classList.remove('modal-personal--show');
 
 }
